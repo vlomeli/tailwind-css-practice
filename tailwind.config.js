37082e086e23cd3e725f5a69051be4cfb2c33b36
vlomeli <victorlomeli2023@gmail.com>
2024-06-26 15:09:34 -0700

    changes to button

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,30 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      'sm': '450px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '576px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '768px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1024px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1680px',
+      // => @media (min-width: 1536px) { ... }
+    },
+
     extend: {
+      colors: {
+        primary: "#ff4800",
+        blue: {
+          '450': "#5f99f7"
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
